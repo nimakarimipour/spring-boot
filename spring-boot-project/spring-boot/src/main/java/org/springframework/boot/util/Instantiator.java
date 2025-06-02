420bd3dc92b6cd83356486dca02a6e3ab763f620
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-01 23:30:37 -0700

    Revert "fix: 61 - DEREFERENCE_NULLABLE - return instantiate(names.stream().map((name) -> TypeSupplier.forName(classLoader, name))); - dereferenced expression names is @Nullable"
    This reverts commit 823bd189bcaaf9e8bfe19da36076df3346b15c4b.

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/util/Instantiator.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/util/Instantiator.java
@@ -36,7 +36,6 @@ import org.springframework.util.Assert;
 import org.springframework.util.ClassUtils;
 import org.springframework.util.ReflectionUtils;
 import javax.annotation.Nullable;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 
 /**
  * Simple factory used to instantiate objects by injecting available parameters.
@@ -123,10 +122,10 @@ public class Instantiator<T> {
 	 * @return a list of instantiated instances
 	 * @since 2.4.8
 	 */
-	public List<T> instantiate(ClassLoader classLoader, Collection<String> names) {
-       Assert.notNull(names, "Names must not be null");
-       return instantiate(NullabilityUtil.castToNonnull(names, "asserted nonnull").stream().map((name) -> TypeSupplier.forName(classLoader, name)));
-   }
+	public List<T> instantiate(@Nullable ClassLoader classLoader, @Nullable Collection<String> names) {
+		Assert.notNull(names, "Names must not be null");
+		return instantiate(names.stream().map((name) -> TypeSupplier.forName(classLoader, name)));
+	}
 
 	/**
 	 * Instantiate the given set of classes, injecting constructor arguments as necessary.

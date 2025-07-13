d0a25673ca58febf99230fba5e24acc7811f43fc
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 15:49:33 -0700

    fix: 64 - DEREFERENCE_NULLABLE - logger.info("Netty RSocket started on port(s): " + address().getPort()); - dereferenced expression address() is @Nullable

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/rsocket/netty/NettyRSocketServer.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/rsocket/netty/NettyRSocketServer.java
@@ -64,11 +64,16 @@ public class NettyRSocketServer implements RSocketServer {
 	}
 
 	@Override
-	public void start() throws RSocketServerException {
-		this.channel = block(this.starter, this.lifecycleTimeout);
-		logger.info("Netty RSocket started on port(s): " + address().getPort());
-		startDaemonAwaitThread(this.channel);
-	}
+   public void start() throws RSocketServerException {
+       this.channel = block(this.starter, this.lifecycleTimeout);
+       InetSocketAddress address = address();
+       if (address != null) {
+           logger.info("Netty RSocket started on port(s): " + address.getPort());
+       } else {
+           logger.warn("Channel is null. Address could not be determined.");
+       }
+       startDaemonAwaitThread(this.channel);
+   }
 
 	private void startDaemonAwaitThread(CloseableChannel channel) {
 		Thread awaitThread = new Thread(() -> channel.onClose().block(), "rsocket");

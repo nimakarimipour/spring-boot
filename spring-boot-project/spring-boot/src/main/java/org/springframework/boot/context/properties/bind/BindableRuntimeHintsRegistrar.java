fe554e5bfa7d21218f585825ebadaaae444e349b
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 08:19:46 -0700

    before:78to77 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression this.bindConstructor is @Nullable' /home/nima/Developer/nullness-benchmarks/spring-boot/spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/bind/BindableRuntimeHintsRegistrar.java:154 			for (int i = 0; i < this.bindConstructor.getParameterCount(); i++) {

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/bind/BindableRuntimeHintsRegistrar.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/bind/BindableRuntimeHintsRegistrar.java
@@ -151,12 +151,14 @@ public class BindableRuntimeHintsRegistrar implements RuntimeHintsRegistrar {
 		}
 
 		private void handleValueObjectProperties(ReflectionHints hints) {
-			for (int i = 0; i < this.bindConstructor.getParameterCount(); i++) {
-				String propertyName = this.bindConstructor.getParameters()[i].getName();
-				ResolvableType propertyType = ResolvableType.forConstructorParameter(this.bindConstructor, i);
-				handleProperty(hints, propertyName, propertyType);
-			}
-		}
+        if (this.bindConstructor != null) {
+            for (int i = 0; i < this.bindConstructor.getParameterCount(); i++) {
+                String propertyName = this.bindConstructor.getParameters()[i].getName();
+                ResolvableType propertyType = ResolvableType.forConstructorParameter(this.bindConstructor, i);
+                handleProperty(hints, propertyName, propertyType);
+            }
+        }
+    }
 
 		private void handleJavaBeanProperties(ReflectionHints hints) {
 			for (PropertyDescriptor propertyDescriptor : this.beanInfo.getPropertyDescriptors()) {

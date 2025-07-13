37d37e5ebf7021a8080b35fe58b95439d18e74fb
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:19:07 -0700

    fix: 62 - DEREFERENCE_NULLABLE - if (bindException.getMessage().toLowerCase().contains("in use")) { - dereferenced expression bindException.getMessage() is @Nullable

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/server/PortInUseException.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/server/PortInUseException.java
@@ -20,6 +20,7 @@ import java.net.BindException;
 import java.util.function.Consumer;
 import java.util.function.IntSupplier;
 import javax.annotation.Nullable;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 
 /**
  * A {@code PortInUseException} is thrown when a web server fails to start due to a port
@@ -80,13 +81,12 @@ public class PortInUseException extends WebServerException {
 	 * @since 2.2.7
 	 */
 	public static void ifPortBindingException(Exception ex, Consumer<BindException> action) {
-		ifCausedBy(ex, BindException.class, (bindException) -> {
-			// bind exception can be also thrown because an address can't be assigned
-			if (bindException.getMessage().toLowerCase().contains("in use")) {
-				action.accept(bindException);
-			}
-		});
-	}
+ 		ifCausedBy(ex, BindException.class, (bindException) -> {
+ 			if (Nullability.castToNonnull(bindException.getMessage()).toLowerCase().contains("in use")) {
+ 				action.accept(bindException);
+ 			}
+ 		});
+ }
 
 	/**
 	 * Perform an action if the given exception was caused by a specific exception type.

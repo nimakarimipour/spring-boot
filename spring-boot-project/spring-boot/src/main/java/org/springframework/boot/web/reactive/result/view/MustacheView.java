5b253e44670e2cede13f98f44b671a0fa130ec98
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:00:22 -0700

    fix: 18 - DEREFERENCE_NULLABLE - Template template = this.compiler.compile(reader); - dereferenced expression this.compiler is @Nullable

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/reactive/result/view/MustacheView.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/reactive/result/view/MustacheView.java
@@ -49,8 +49,8 @@ import javax.annotation.Nullable;
  */
 public class MustacheView extends AbstractUrlBasedView {
 
-	@Nullable
-	private Compiler compiler;
+	
+	@Nullable private Compiler compiler;
 
 	@Nullable
 	private String charset;
@@ -79,28 +79,29 @@ public class MustacheView extends AbstractUrlBasedView {
 	}
 
 	@Override
-	protected Mono<Void> renderInternal(Map<String, Object> model, MediaType contentType, ServerWebExchange exchange) {
-		Resource resource = resolveResource();
-		if (resource == null) {
-			return Mono
-					.error(new IllegalStateException("Could not find Mustache template with URL [" + getUrl() + "]"));
-		}
-		DataBuffer dataBuffer = exchange.getResponse().bufferFactory()
-				.allocateBuffer(DefaultDataBufferFactory.DEFAULT_INITIAL_CAPACITY);
-		try (Reader reader = getReader(resource)) {
-			Template template = this.compiler.compile(reader);
-			Charset charset = getCharset(contentType).orElseGet(this::getDefaultCharset);
-			try (Writer writer = new OutputStreamWriter(dataBuffer.asOutputStream(), charset)) {
-				template.execute(model, writer);
-				writer.flush();
-			}
-		}
-		catch (Exception ex) {
-			DataBufferUtils.release(dataBuffer);
-			return Mono.error(ex);
-		}
-		return exchange.getResponse().writeWith(Flux.just(dataBuffer));
-	}
+   protected Mono<Void> renderInternal(Map<String, Object> model, MediaType contentType, ServerWebExchange exchange) {
+       if (this.compiler == null) {
+           return Mono.error(new IllegalStateException("Compiler must not be null"));
+       }
+       Resource resource = resolveResource();
+       if (resource == null) {
+           return Mono.error(new IllegalStateException("Could not find Mustache template with URL [" + getUrl() + "]"));
+       }
+       DataBuffer dataBuffer = exchange.getResponse().bufferFactory()
+               .allocateBuffer(DefaultDataBufferFactory.DEFAULT_INITIAL_CAPACITY);
+       try (Reader reader = getReader(resource)) {
+           Template template = this.compiler.compile(reader);
+           Charset charset = getCharset(contentType).orElseGet(this::getDefaultCharset);
+           try (Writer writer = new OutputStreamWriter(dataBuffer.asOutputStream(), charset)) {
+               template.execute(model, writer);
+               writer.flush();
+           }
+       } catch (Exception ex) {
+           DataBufferUtils.release(dataBuffer);
+           return Mono.error(ex);
+       }
+       return exchange.getResponse().writeWith(Flux.just(dataBuffer));
+   }
 
 	@Nullable
 	private Resource resolveResource() {

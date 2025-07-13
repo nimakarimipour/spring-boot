10e0d154bbd64c7a9df52ea8fdf84bfbdbd00f7a
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 16:11:14 -0700

    Revert "fix: 37 - DEREFERENCE_NULLABLE - this.shutdownThread.interrupt(); - dereferenced expression this.shutdownThread is @Nullable"
    This reverts commit 365ab5531f4c51466947bc0f5349dc571a83a3be.

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/embedded/netty/GracefulShutdown.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/embedded/netty/GracefulShutdown.java
@@ -26,7 +26,6 @@ import reactor.netty.DisposableServer;
 import org.springframework.boot.web.server.GracefulShutdownCallback;
 import org.springframework.boot.web.server.GracefulShutdownResult;
 import javax.annotation.Nullable;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 
 /**
  * Handles Netty graceful shutdown.
@@ -76,14 +75,14 @@ final class GracefulShutdown {
 	}
 
 	void abort() {
- 		Thread shutdownThread = this.shutdownThread;
- 		if (shutdownThread != null) {
- 			while (!this.shuttingDown) {
- 				sleep(50);
- 			}
- 			Nullability.castToNonnull(this.shutdownThread, "already checked not null").interrupt();
- 		}
- }
+		Thread shutdownThread = this.shutdownThread;
+		if (shutdownThread != null) {
+			while (!this.shuttingDown) {
+				sleep(50);
+			}
+			this.shutdownThread.interrupt();
+		}
+	}
 
 	private void sleep(long millis) {
 		try {

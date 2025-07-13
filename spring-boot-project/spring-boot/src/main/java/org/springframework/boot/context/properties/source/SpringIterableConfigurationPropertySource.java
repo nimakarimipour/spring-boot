36059ba3458987c1a52303408ff1a404d31f5a73
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 15:52:33 -0700

    Revert "fix: 79 - METHOD_NO_INIT - Mappings(PropertyMapper[] mappers, boolean immutable, boolean trackDescendants) { - initializer method does not guarantee @NonNull fields mappings (line 199), descendants (line 204) are initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit a1f6359a4468dcbb460ad5079d4d2ee6455b1adc.

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/source/SpringIterableConfigurationPropertySource.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/source/SpringIterableConfigurationPropertySource.java
@@ -210,15 +210,10 @@ class SpringIterableConfigurationPropertySource extends SpringConfigurationPrope
 		private volatile String[] lastUpdated;
 
 		Mappings(PropertyMapper[] mappers, boolean immutable, boolean trackDescendants) {
-        this.mappers = mappers;
-        this.immutable = immutable;
-        this.trackDescendants = trackDescendants;
-        this.mappings = new HashMap<>();
-        this.reverseMappings = new HashMap<>();
-        this.descendants = new HashMap<>();
-        this.configurationPropertyNames = new ConfigurationPropertyName[0];
-        this.lastUpdated = new String[0];
-    }
+			this.mappers = mappers;
+			this.immutable = immutable;
+			this.trackDescendants = trackDescendants;
+		}
 
 		void updateMappings(Supplier<String[]> propertyNames) {
 			if (this.mappings == null || !this.immutable) {

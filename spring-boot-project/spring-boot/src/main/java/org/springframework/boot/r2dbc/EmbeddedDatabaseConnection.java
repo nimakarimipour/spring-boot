90ec3f09c83fad65d18d40460a85ca9b536916cb
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 08:25:55 -0700

    before:59to58 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression optionsCapable is @Nullable' /home/nima/Developer/nullness-benchmarks/spring-boot/spring-boot-project/spring-boot/src/main/java/org/springframework/boot/r2dbc/EmbeddedDatabaseConnection.java:112 		ConnectionFactoryOptions options = optionsCapable.getOptions();

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/r2dbc/EmbeddedDatabaseConnection.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/r2dbc/EmbeddedDatabaseConnection.java
@@ -104,19 +104,20 @@ public enum EmbeddedDatabaseConnection {
 	 * @since 2.5.1
 	 */
 	public static boolean isEmbedded(ConnectionFactory connectionFactory) {
-		OptionsCapableConnectionFactory optionsCapable = OptionsCapableConnectionFactory.unwrapFrom(connectionFactory);
-		Assert.notNull(optionsCapable,
-				() -> "Cannot determine database's type as ConnectionFactory is not options-capable. To be "
-						+ "options-capable, a ConnectionFactory should be created with "
-						+ ConnectionFactoryBuilder.class.getName());
-		ConnectionFactoryOptions options = optionsCapable.getOptions();
-		for (EmbeddedDatabaseConnection candidate : values()) {
-			if (candidate.embedded.test(options)) {
-				return true;
-			}
-		}
-		return false;
-
-	}
+       OptionsCapableConnectionFactory optionsCapable = OptionsCapableConnectionFactory.unwrapFrom(connectionFactory);
+       if (optionsCapable == null) {
+           throw new IllegalArgumentException(
+                   "Cannot determine database's type as ConnectionFactory is not options-capable. To be "
+                   + "options-capable, a ConnectionFactory should be created with "
+                   + ConnectionFactoryBuilder.class.getName());
+       }
+       ConnectionFactoryOptions options = optionsCapable.getOptions();
+       for (EmbeddedDatabaseConnection candidate : values()) {
+           if (candidate.embedded.test(options)) {
+               return true;
+           }
+       }
+       return false;
+   }
 
 }
